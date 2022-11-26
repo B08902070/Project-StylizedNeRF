d1335eba95f116d01606afa811ef526c6cdb6928
B08902070 <72483821+B08902070@users.noreply.github.com>
2022-11-26 15:06:05 +0800

    add style_module in  nerf_related_module

--- nerf_related_modules.py
+++ nerf_related_modules.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 
 
 class Sine(nn.Module):
-    def __init(self, w0=30.):
+    def __init__(self, w0=30.):
         super().__init__()
         self.w0 = w0
 
@@ -57,7 +57,7 @@ class Embedder:
         return out
       
 
-class Style_MLP(nn.Module):
+class Style_NeRF_MLP(nn.Module):
     def __init__(self, W=8, D=256, input_ch_pts=3, input_ch_viewdir=3, skips=[4],
                      act_fn=nn.ReLU, use_viewdir=True, sigma_mul=0, enable_style=False):
         super().__init__()
@@ -148,7 +148,7 @@ class Style_NeRF(nn.Module):
         else:
             self.netdepth = args.netdepth_fine
             self.netwidth = args.netwidth_fine
-        self.net = Style_MLP(W=self.netwidth, D=self.netdepth, input_ch_pts=self.input_ch_pts, input_ch_viewdir=self.input_ch_viewdir,
+        self.net = Style_NeRF_MLP(W=self.netwidth, D=self.netdepth, input_ch_pts=self.input_ch_pts, input_ch_viewdir=self.input_ch_viewdir,
                              skips=self.skips, act_fn=self.act_fn, use_viewdir=self.use_viewdir, sigma_mul=self.sigma_mul, enable_style=enable_style)        
 
     def set_enable_style(self, enable_style):
@@ -162,4 +162,35 @@ class Style_NeRF(nn.Module):
 
         out = self.net(pts, dirs)
         out['dirs'] = dirs
+        return out
+
+class Style_Module(nn.Module):
+    def __init__(self, args, mode='coarse'):
+        super().__init__()
+        self.D = args.style_D
+        self.W = args.netwith
+        self.input_ch = args.embed_freq_coor * 3 * 2 + 3 + args.vae_latent
+        self.skips = [4]
+        self.layers=nn.ModuleList()
+        cur_dim = self.input_ch
+        for i in range(self.D-1):
+            if i in self.skips:
+                dim += args.embed_freq_coor*3*2+3
+            self.layers.append(nn.Linear(cur_dim, self.W))
+            cur_dim = self.W
+        self.layers.append(nn.Linear(cur_dim, 3))
+
+    def forward(self, **kwargs):
+        x = kwargs['x']
+        l = kwargs['latent']
+        h = x
+        for i in range(len(self.layers)-1):
+            h = torch.cat((h, l), dim=-1)
+            if i in self.skips:
+                h = torch.cat((h, x), dim=-1)
+            h = F.relu(self.layers[i](h))
+        h = torch.cat((h, l), dim=-1)
+        h = self.layers[-1](h)
+        h = F.sigmoid(h)
+        out = {'rgb': h}
         return out
