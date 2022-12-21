3454f734aaa3ac1ee9362cce7671badeb4dc8bb5
B08902070 <72483821+B08902070@users.noreply.github.com>
2022-12-21 20:12:46 +0800

    fix tensor movedim

--- dataset_helper.py
+++ dataset_helper.py
@@ -156,7 +156,9 @@ def style_data_prepare(style_path, content_images, size=512, chunk=64, sv_path=N
         start = 0
         while start < content_images.shape[0]:
             end = min(start + chunk, content_images.shape[0])
-            tmp_imgs = torch.Tensor([img_trans(img) for img in content_images[start:end]])
+            tmp_imgs = torch.Tensor([])
+            for img in content_images[start:end]:
+                tmp_imgs = torch.cat([tmp_imgs, img_trans(img).unsqueeze_(0)])
             tmp_imgs = torch.movedim(tmp_imgs.float().to(device), -1, 1)
             with torch.no_grad():
                 _, _, tmp_stylized_imgs, tmp_style_features = nst_net(content_img=tmp_imgs, style_img=style_img[:tmp_imgs.shape[0]], alpha=1, return_img_and_feat = True)
